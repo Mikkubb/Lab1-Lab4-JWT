b98d75cf1f97871111e3e5bdb66b41171847b050
Jakub Michalski <Jakub.Michalski@microsoft.wsei.edu.pl>
2023-07-08 04:30:20 +0200

    Completed Lab1

--- StudLab/Program.cs
+++ StudLab/Program.cs
@@ -1,13 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using StudLab.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StudentContext>(opt =>opt.UseInMemoryDatabase("StudentList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Student API",
+        Description = "demo Student API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://adres_strony")
+        },
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
 
 var app = builder.Build();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Student API V1");
+});
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
